edf8ea52923840ed632c78a8c0073daa091d33a4
Gonzalo <glopez@tibco.com>
2020-01-31 14:21:48 +0100

    Adding dummy function

---
+++ extensions/functions/number/random2.go
@@ -0,0 +1,38 @@
+package number
+
+import (
+	"github.com/project-flogo/core/data/coerce"
+	"math/rand"
+	"time"
+
+	"github.com/project-flogo/core/data"
+	"github.com/project-flogo/core/data/expression/function"
+)
+
+func init() {
+	_ = function.Register(&fnRandom2{})
+}
+
+type fnRandom2 struct {
+}
+
+func (fnRandom2) Name() string {
+	return "random"
+}
+
+func (fnRandom2) Sig() (paramTypes []data.Type, isVariadic bool) {
+	return []data.Type{data.TypeInt}, true
+}
+
+func (fnRandom2) Eval(params ...interface{}) (interface{}, error) {
+	limit := 10
+	if len(params) > 0 {
+		var err error
+		limit, err = coerce.ToInt(params[0])
+		if err != nil {
+			limit = 10
+		}
+	}
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(limit), nil
+}
